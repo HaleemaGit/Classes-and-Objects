a0e171ed5176dc24edd169d6408eb4a0ab36387f
HaleemaGit <mustaphahaleema3@gmail.com>
2022-05-27 20:05:03 +0100

    little update made to file

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 class Student:
-    
+
+    # [assignment] Skeleton class. Add your code here
+
     def __init__(self, name: str, age: int, tracks: [], score: float):
+
         self.name = name
         self.age = age
         self.score = score
-        self.tracks = [] 
-               
+        self.tracks = tracks
+
     def change_name(self, name: str):
         self.name = name
         print(self.name)
+
     def change_age(self, age: int):
         self.age = age
         print(self.age)
+
     def add_track(self, track:str):
         self.tracks.append(track)
         print(self.tracks)
+
     def get_score(self):
         print(self.score)
-            
-           
+
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
 # Expected methods
-Bob.change_name("Rufaydah")
-Bob.change_age(12)
-Bob.add_track("Project Manager")
+Bob.change_name("Peter")
+
+Bob.change_age(34)
+
+Bob.add_track("UI/UX")
+
 Bob.get_score()
-
-
-
